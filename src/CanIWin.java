527db33d34d9a418886f3f34edd4c9b9d159670b
sophiesongge <sophiesongge@gmail.com>
2016-12-06 12:00:30 +0100

    src/CanIWin.java

--- src/CanIWin.java
+++ src/CanIWin.java
@@ -1,4 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CanIWin{
+	Map<Integer, Boolean> map;
+	boolean[] used;
+	
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
         int sum = (1+maxChoosableInteger)*maxChoosableInteger/2;
         if(desiredTotal > sum){
@@ -7,13 +13,39 @@ public class CanIWin{
         if(desiredTotal <= 0){
             return true;
         }
-        boolean[] dp = new boolean[maxChoosableInteger+1];
-        dp[0] = true;
-        dp[1] = true;
-        
-        
-        return false;
+        map = new HashMap();
+        used = new boolean[maxChoosableInteger+1];
+        return helper(desiredTotal);
     }
     
-    public boolean helper(int maxChoosableInteger, int desiredTotal, )
+    public boolean helper(int desiredTotal){
+    	if(desiredTotal <= 0){
+    		return false;
+    	}
+    	int mapkey = format(used);
+    	if(!map.containsKey(mapkey)){
+    		for(int i=1; i<used.length; i++){
+    			used[i] = true;
+    			if(!helper(desiredTotal - i)){
+    				map.put(mapkey, true);
+    				used[i] = false;
+    				return true;
+    			}
+    			used[i] = false;
+    		}
+    		map.put(mapkey, false);
+    	}
+    	return map.get(mapkey);
+    }
+    
+    public int format(boolean[] used){
+    	int num = 0;
+    	for(boolean b : used){
+    		num <<= 1;
+    		if(b) {
+    			num |= 1;
+    		}
+    	}
+    	return num;
+    }
 }
