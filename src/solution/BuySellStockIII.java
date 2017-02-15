c54de8003c9d34743a02567621fc3f2badde6fdb
sophiesongge <sophiesongge@gmail.com>
2017-02-15 17:05:02 +0100

    changes in BuySellStockIII

--- src/solution/BuySellStockIII.java
+++ src/solution/BuySellStockIII.java
@@ -6,8 +6,10 @@
  * 而local[i][j]表示到第i天最多进行j次交易，并且在第i天卖出可以达到的最大利润。
  * 则： global[i][j] = Math.max(local[i][j], global[i-1][j]) 也就是到今天为止的全局最好，是到今天为止的局部最好与到昨天的全局最好中大的那个
  * 因为今天的最大利润有两种情况，第一是几天卖出了（既local[i][j]），另一种是今天什么都没做，那今天的最好的情况就是昨天的全局最好情况
- * 而： local[i][j] = Math.max(global[i-1][j-1] + prices[i] - prices[i-1], local[i-1][j-1] + prices[i] - prices[i-1])
+ * 而： local[i][j] = Math.max(global[i-1][j-1] + prices[i] - prices[i-1], local[i-1][j] + prices[i] - prices[i-1]) --- 这里是local[i-1][j]因为就算第i-1天卖了， 可以再买回来在第i天再卖，所以并不增加transaction
  * 这里就不太好理解了， 因为今天需要卖出，所以需要加prices[i] - prices[i-1], 两种加的可能性，（1） 和全局比，（2）和局部比
+ * 
+ * 如果只是两次操作，相当于j = 2，而且空间上维护当天数据即可，不需要i
  */
 
 package solution;
