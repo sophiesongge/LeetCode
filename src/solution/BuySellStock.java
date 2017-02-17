39468f7aa87ab5a669c05aeecc03d2ee6bc453b6
sophiesongge <sophiesongge@gmail.com>
2017-02-17 11:27:43 +0100

    small changes

--- src/solution/BuySellStock.java
+++ src/solution/BuySellStock.java
@@ -6,6 +6,7 @@ package solution;
 
 public class BuySellStock {
 	
+	//贪心
 	public int maxProfit(int[] prices) {
 		if(prices == null || prices.length <= 1) {
 			return 0;
@@ -21,31 +22,20 @@ public class BuySellStock {
 		return max;
 	}
 	
+	//动态规划
 	public int maxProfit0(int[] prices) {
-		if(prices == null || prices.length == 0){
-			return 0;
-		}
-		
-		int len = prices.length;
-		int[] dp = new int[len+1];
-		dp[0] = 0;
-		dp[1] = 0;
-		
-		int min = prices[0];
-		int max = 0;
-		
-		for(int i=2; i<=len; i++){			
-			if(prices[i-1] < min){
-				min = prices[i-1];
-			}
-			if(prices[i-1] - min > max){
-				dp[i] = prices[i-1] - min;
-				max = dp[i];
-			}else{
-				dp[i] = max;
-			}
-		}
-		
-		return dp[len];
+        if(prices == null || prices.length <= 1){
+            return 0;
+        }
+        int len = prices.length;
+        int[] dp = new int[len+1];
+        
+        int min = prices[0];
+        
+        for(int i=2; i<=len; i++) {
+            min = Math.min(min, prices[i-1]);
+            dp[i] = Math.max(dp[i-1], prices[i-1] - min);
+        }
+        return dp[len];
 	}
 }
