3b28e0883b2ea1fb0568fd3cf98da13ab3e9a92f
sophiesongge <sophiesongge@gmail.com>
2017-03-02 16:58:10 +0100

    I am blocked by this

--- src/solution/TrappingRainWater.java
+++ src/solution/TrappingRainWater.java
@@ -2,6 +2,9 @@ package solution;
 
 /**
  * Created by sophiesong on 01/03/2017.
+ * @author Sophie
+ * Trapping Rain Water: https://leetcode.com/problems/trapping-rain-water/
+ *
  */
 public class TrappingRainWater {
 
