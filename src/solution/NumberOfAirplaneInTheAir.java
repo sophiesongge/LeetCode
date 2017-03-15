4be8bc821dfe21bfe40be3569df335953a93c2a3
sophiesongge <sophiesongge@gmail.com>
2017-03-15 11:59:49 +0100

    add comments for number of airplane in the air

--- src/solution/NumberOfAirplaneInTheAir.java
+++ src/solution/NumberOfAirplaneInTheAir.java
@@ -23,11 +23,13 @@ public class NumberOfAirplaneInTheAir {
         int count = 0;
         int ans = 0;
         for(Point p : list) {
+            //遇到一个start点，就把count + 1， 相反遇到一个end点，就把count - 1
             if(p.flag == 1) {
                 count++;
             }else{
                 count--;
             }
+            //最终的结果
             ans = Math.max(ans, count);
         }
         return ans;
@@ -36,13 +38,14 @@ public class NumberOfAirplaneInTheAir {
 
 class Point {
     int time;
-    int flag;
+    int flag;//1 == start time, 0 == end time
 
-    Point(int t, int s) {
-        this.time = t;
-        this.flag = s;
+    Point(int time, int flag) {
+        this.time = time;
+        this.flag = flag;
     }
 
+    //对所有的区间的起始点按大小排序（由小到大），如果两个点一样，则把开始的点放前面
     public static Comparator<Point> PointComparator = new Comparator<Point>(){
         public int compare(Point p1, Point p2) {
             if(p1.time == p2.time) {
