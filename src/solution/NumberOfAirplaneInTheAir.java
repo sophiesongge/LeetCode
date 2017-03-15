ddea762557a4e7410a94347997782ecf760df1df
sophiesongge <sophiesongge@gmail.com>
2017-03-15 12:05:01 +0100

    add comments for number of airplane in the air

--- src/solution/NumberOfAirplaneInTheAir.java
+++ src/solution/NumberOfAirplaneInTheAir.java
@@ -23,13 +23,14 @@ public class NumberOfAirplaneInTheAir {
         int count = 0;
         int ans = 0;
         for(Point p : list) {
-            //遇到一个start点，就把count + 1， 相反遇到一个end点，就把count - 1
+            //遇到一个start点，说明两个灰机在天上重叠了，所以就把count + 1； 相反遇到一个end点，可以理解为一个重叠区域结束了，就把count - 1
             if(p.flag == 1) {
                 count++;
             }else{
                 count--;
             }
-            //最终的结果
+            //在当前状态下，结果应该是在当前这个点的位置，重叠的飞机有多少，如果当前点是个start点，则这个数目应该是count的数目，如果当前点是个end点，则这个数目则应该是前一状态的点数，前一状态又是由ans记录的，所以
+            //总体来说ans应该是ans和count中大的那个
             ans = Math.max(ans, count);
         }
         return ans;
