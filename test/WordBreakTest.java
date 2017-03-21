5765cc4115972a714834d85e57b0d56687bc9b45
sophiesongge <sophiesongge@gmail.com>
2017-03-21 17:30:02 +0100

    fix test|

--- test/WordBreakTest.java
+++ test/WordBreakTest.java
@@ -1,14 +1,13 @@
-package test;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import solution.WordBreak;
-
 import org.junit.Test;
+
+import solution.WordBreak;
 
 public class WordBreakTest {
 	
