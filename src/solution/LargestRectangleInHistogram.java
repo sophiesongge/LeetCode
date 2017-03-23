c44e45e9cf4ccde17773cb839586d1d6c2413058
sophiesongge <sophiesongge@gmail.com>
2017-03-23 15:25:03 +0100

    right answer for Largest Rectangle in Histogram

--- src/solution/LargestRectangleInHistogram.java
+++ src/solution/LargestRectangleInHistogram.java
@@ -1,9 +1,13 @@
 package solution;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * @author Sophie Song
  * @since 21/03/2017
  * Largest Rectangle in Histogram: https://leetcode.com/problems/largest-rectangle-in-histogram/?tab=Description#/solutions
+ * 非常好的一道stack的题！思路非常巧妙，我自己只写出了一个基础的TLE方法。
  */
 public class LargestRectangleInHistogram {
 
@@ -38,6 +42,35 @@ public class LargestRectangleInHistogram {
 
             maxSurface = Math.max(maxSurface, surface);
             //System.out.println("%%%%%%%%%%" + heights[i] + " : " + maxSurface + "%%%%%%%%%%%");
+        }
+        return maxSurface;
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        if(heights == null || heights.length == 0) {
+            return 0;
+        }
+
+        //搞一个stack用来排序，如果当前元素比stack最后栈顶的元素大就压进栈；要不然就出栈，知道比栈顶元素小为止；
+        //栈里装的是index，而不是具体的高度
+        Stack<Integer> stack = new Stack<>();
+
+        int i = 0;
+        int maxSurface = 0;
+
+        int[] h = new int[heights.length + 1];
+        h = Arrays.copyOf(heights, heights.length + 1);
+
+        while(i<h.length) {
+            if(stack.isEmpty() || h[stack.peek()] <= h[i]) {
+                stack.push(i);
+                i++;
+            }else {
+                //如果栈顶元素比当前元素大，就让栈顶元素出栈，因为这个时候栈顶元素就不能喝当前元素组成矩形了，出了栈之后，为这个出栈的元素算面积
+                int index = stack.pop();
+                //这里是比较难理解的点，算面积的时候，只有前后的高度都比当前大，才能算，而从栈顶元素后面的一位开始一直到i都是比刚出栈的这个元素大的
+                maxSurface = Math.max(maxSurface, h[index] * (stack.isEmpty() ? i : i - stack.peek() - 1));
+            }
         }
         return maxSurface;
     }
--- test/LargestRectangleInHistogramTest.java
+++ test/LargestRectangleInHistogramTest.java
@@ -17,21 +17,19 @@ public class LargestRectangleInHistogramTest {
     public void largestRectangleInHistogramTest() {
         int[] heights = null;
 
-        assertEquals(0, test.largestRectangleArea0(heights));
+        assertEquals(test.largestRectangleArea0(heights), test.largestRectangleArea(heights));
 
         heights = new int[]{2, 1, 5, 6, 2, 3};
 
-        assertEquals(10, test.largestRectangleArea0(heights));
+        assertEquals(test.largestRectangleArea0(heights), test.largestRectangleArea(heights));
 
         heights = new int[]{4, 2, 0, 3, 2, 5};
 
-        assertEquals(6, test.largestRectangleArea0(heights));
+        assertEquals(test.largestRectangleArea0(heights), test.largestRectangleArea(heights));
 
         heights = new int[]{4,2,0,3,2,4,3,4};
 
-        assertEquals(10, test.largestRectangleArea0(heights));
-
-        //System.out.println(test.largestRectangleArea(heights));
+        assertEquals(test.largestRectangleArea0(heights), test.largestRectangleArea(heights));
 
     }
 }
