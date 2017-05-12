2f943c9e80596ee6dc6c344ce1ad9e239f5e4c0a
sophiesongge <sophiesongge@gmail.com>
2017-05-12 14:42:59 +0200

    final right answer

--- src/solution/ReversePairs.java
+++ src/solution/ReversePairs.java
@@ -110,48 +110,63 @@ public class ReversePairs {
 
 //这个方法还是MergeSort，这个好理解很多
 
-    public void merge(int[] nums, int start, int mid, int end) {
+
+    private void merge(int[] nums, int start, int mid, int end) {
         int[] helper = new int[nums.length];
         for(int i=0; i<nums.length; i++) {
             helper[i] = nums[i];
         }
 
         int left = start;
-        int right = mid+1;
+        int right = mid + 1;
         int index = start;
 
         while(left <= mid && right <= end) {
             if(helper[left] < helper[right]) {
                 nums[index++] = helper[left++];
-            }else {
+            }else{
                 nums[index++] = helper[right++];
             }
         }
-        while(left<=mid) {
-            nums[index++] = nums[left++];
+
+        while(left <= mid) {
+            nums[index++] = helper[left++];
         }
     }
 
-    public int mergeSort(int[] nums, int start, int end) {
-        if(start > end) {
+    private int mergeSort(int[] nums, int start, int end) {
+        if(start >= end) {
             return 0;
         }
 
-        int mid = start + (end - start)/2;
-
+        int mid = start + (end - start) / 2;
         int count = mergeSort(nums, start, mid) + mergeSort(nums, mid+1, end);
 
-        for(int left = start, right = mid+1; left <= mid; left++) {
+        for(int left = start, right = mid+1; left<=mid; left++) {
             while(right <= end && nums[left]/2.0 > nums[right]) {
                 right++;
             }
             count = count + right - (mid + 1);
         }
+        merge(nums, start, mid, end);
+
         return count;
     }
+
 
     public int reversePairs(int[] nums) {
         return mergeSort(nums, 0, nums.length-1);
     }
 
+    public static void main(String[] args) {
+        ReversePairs test = new ReversePairs();
+
+        int[] nums = new int[]{1, 3, 2, 3, 1};
+
+        System.out.println(test.reversePairs(nums));
+
+        nums = new int[]{2,4,3,5,1};
+        System.out.println(test.reversePairs(nums));
+
+    }
 }
