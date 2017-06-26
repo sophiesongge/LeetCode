01c77d5c0e961accc8dc6d1610be946ec9e43cd9
sophiesongge <sophiesongge@gmail.com>
2017-06-26 18:15:57 +0200

    LFUCache not perfect version

--- src/solution/LFUCache.java
+++ src/solution/LFUCache.java
@@ -1,86 +1,106 @@
 package solution;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.Comparator;
 
-class Cache{
-	int key;
-	int frequency;
-	int recent;
-	public Cache(int k, int f, int r){
-		this.key = k;
-		this.frequency = f;
-		this.recent = r;
-	}
-}
+
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 public class LFUCache {
     public int capacity;
-    private HashMap<Integer, Integer> cache;
-    private HashMap<Integer, Integer> frequency;
-    private PriorityQueue<Integer> queue;
-    
-    public static void main(String[] args){
-    	LFUCache lfu = new LFUCache(2);
-    	System.out.println(lfu.get(1));
-    	lfu.set(1, 1);
-    	lfu.set(2, 2);
-    	System.out.println(lfu.get(1));
-    	lfu.set(3, 3);
-    	System.out.println(lfu.get(1));
-    	lfu.set(4, 4);
-    	System.out.println(lfu.get(2));
-    	System.out.println(lfu.get(3));
-    }
+    private Map<Integer, LFUNode> map;
+    private PriorityQueue<LFUNode> queue;
 
     public LFUCache(int capacity) {
         this.capacity = capacity;
-        this.cache = new HashMap();
-        this.frequency = new HashMap();
-        Comparator<Integer> cmp = new Comparator<Integer>(){
-            public int compare(Integer e1, Integer e2){
-            	if(frequency.containsKey(e1) && frequency.containsKey(e2)){
-            		return frequency.get(e1) - frequency.get(e2);
-            	}else{
-            		return 0;
-            	}
+        this.map = new HashMap<>();
+        Comparator<LFUNode> cmp = new Comparator<LFUNode>() {
+            @Override
+            public int compare(LFUNode o1, LFUNode o2) {
+                if(o1.frequency != o2.frequency){
+                    return o1.frequency - o2.frequency;
+                }else{
+                    return (int) ((int) o1.timestamp - o2.timestamp)*10000000;
+                }
+
+
+
             }
         };
-        this.queue = new PriorityQueue(10, cmp);
+        this.queue = new PriorityQueue<>(10, cmp);
     }
-    
+
     public int get(int key) {
-        if(cache.containsKey(key)){
-            frequency.put(key, frequency.get(key) + 1);
-            queue.add(key);
-            return cache.get(key);
+        if(map.containsKey(key)){
+            LFUNode n = map.get(key);
+            queue.remove(n);
+            n.frequency++;
+            n.timestamp = System.currentTimeMillis();
+            map.put(key, n);
+            queue.add(n);
+            return n.value;
         }else{
             return -1;
         }
     }
-    
-    public void set(int key, int value) {
-        if(cache.containsKey(key)){
-            cache.put(key, value);
-            frequency.put(key, frequency.get(key)+1);
-            queue.add(key);
+
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            LFUNode n = map.get(key);
+            queue.remove(n);
+            n.value = value;
+            n.frequency++;
+            n.timestamp = System.currentTimeMillis();
+            map.put(key, n);
+            queue.add(n);
         }else{
-            if(cache.size() < capacity){
-                cache.put(key, value);
-                frequency.put(key, 1);
-                queue.add(key);
+            LFUNode n = new LFUNode(key, value);
+            if(map.size() < capacity) {
+                map.put(key, n);
+                queue.add(n);
             }else{
-                int i = cache.size() - capacity;
-                while(i >= 0){
-                    cache.remove(queue.peek());
-                    frequency.remove(queue.peek());
-                    queue.poll();
-                    i--;
+                if(capacity == 0){
+                    return;
+                }else{
+                    LFUNode last = queue.poll();
+                    map.remove(last.key);
+                    map.put(key, n);
+                    queue.add(n);
                 }
-                cache.put(key, value);
-                frequency.put(key, 1);
-                queue.add(key);
             }
         }
+    }
+
+    public static void main(String[] args){
+        LFUCache lfu = new LFUCache(3);
+        lfu.put(1, 1);
+        lfu.put(2, 2);
+        lfu.put(3, 3);
+        lfu.put(4, 4);
+        System.out.println(lfu.get(4));
+        System.out.println(lfu.get(3));
+        System.out.println(lfu.get(2));
+        System.out.println(lfu.get(1));
+        lfu.put(5, 5);
+        System.out.println(lfu.get(1));
+        System.out.println(lfu.get(2));
+        System.out.println(lfu.get(3));
+        System.out.println(lfu.get(4));
+        System.out.println(lfu.get(5));
+    }
+}
+
+class LFUNode{
+    int key;
+    int value;
+    int frequency;
+    long timestamp;
+
+    public LFUNode(int key, int value) {
+        this.key = key;
+        this.value = value;
+        this.frequency = 1;
+        this.timestamp = System.currentTimeMillis();
     }
 }
