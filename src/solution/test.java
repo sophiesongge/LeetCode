b05667a3cb5b84989bc86d005d8358e07ac2bd8d
sophiesongge <sophiesongge@gmail.com>
2018-07-02 16:30:54 +0200

    test

--- src/solution/test.java
+++ src/solution/test.java
@@ -1,68 +1,92 @@
 package solution;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @author Sophie Song
  * @since 2017/5/10
  */
 public class test {
 
-    public void merge(int[] nums, int start, int mid, int end) {
-        int[] temps = new int[nums.length];
+    public static void main(String[] args){
+        TreeNodeB root = new TreeNodeB(1);
+        TreeNodeB n2 = new TreeNodeB(2);
+        TreeNodeB n3 = new TreeNodeB(5);
+        TreeNodeB n4 = new TreeNodeB(3);
+        TreeNodeB n5 = new TreeNodeB(4);
+        TreeNodeB n6 = new TreeNodeB(6);
+        TreeNodeB n7 = new TreeNodeB(7);
 
-        for(int i=0; i<nums.length; i++) {
-            temps[i] = nums[i];
+        root.left = n2;
+        root.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.right = n6;
+        n6.left = n7;
+
+        BFS(root);
+        DFS(root);
+
+    }
+
+    public static void BFS(TreeNodeB root){
+        if(root == null){
+            return;
         }
 
-        int leftIndex = start;
-        int rightIndex = mid + 1;
-        int index = start;
+        Queue<TreeNodeB> q = new LinkedList<TreeNodeB>();
+        q.add(root);
+        System.out.print(root.val+" ");
 
-        while(leftIndex <= mid && rightIndex <= end) {
-            if(temps[leftIndex] <= temps[rightIndex]) {
-                nums[index++] = temps[leftIndex++];
-            }else {
-                nums[index++] = temps[rightIndex++];
+        while(!q.isEmpty()){
+            TreeNodeB top = q.poll();
+            if(top.left != null){
+                q.add(top.left);
+                System.out.print(top.left.val+" ");
+            }
+            if(top.right != null){
+                q.add(top.right);
+                System.out.print(top.right.val+" ");
             }
         }
-
-        while(leftIndex <= mid) {
-            nums[index++] = temps[leftIndex++];
-        }
-    }
-
-    public void sort(int[] nums, int start, int end) {
-        if(start < end) {
-            int mid = start + (end - start)/2;
-            sort(nums, start, mid);
-            sort(nums, mid+1, end);
-            merge(nums, start, mid, end);
-        }
-    }
-
-    public void mergeSort(int[] nums) {
-        sort(nums, 0, nums.length-1);
-    }
-
-    public static void main(String[] args){
-
-        System.out.println("!!!! " + (3 + (4-3)/2));
-
-        int n = 100;
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i] = n-i-1;
-            System.out.print(arr[i]+" ");
-        }
-
-        test t = new test();
-
         System.out.println();
+    }
 
-        t.mergeSort(arr);
-
-        for(int i=0; i<n; i++){
-            System.out.print(arr[i]+" ");
+    public static void DFS(TreeNodeB root) {
+        if(root == null) {
+            return;
         }
 
+        Stack<TreeNodeB> s = new Stack<>();
+        s.push(root);
+
+        while(!s.isEmpty()) {
+            TreeNodeB top = s.pop();
+            System.out.println(top.val);
+            if(top.right != null) {
+                s.push(top.right);
+            }
+            if(top.left != null) {
+                s.push(top.left);
+            }
+        }
+    }
+}
+
+class TreeNodeB{
+    public int val;
+    public TreeNodeB left;
+    public TreeNodeB right;
+
+    public TreeNodeB(int data){
+        this.val = data;
+        this.left = null;
+        this.right = null;
     }
 }
